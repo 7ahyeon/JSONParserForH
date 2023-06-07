65bd48f75bfb59f8b760b1a36a8806806e9fdcf2
7ahyeon <7ahyeon@gmail.com>
2023-06-08 00:02:17 +0900

    JSONFileReader 수정

--- src/main/java/com/hanhwa/jsonparser/JSONFileReader.java
+++ src/main/java/com/hanhwa/jsonparser/JSONFileReader.java
@@ -1,22 +1,37 @@
 package com.hanhwa.jsonparser;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class JSONFileReader {
     private String jsonFileContent;
 
-    public String read(String jsonFilePath) {
-        // jsonFilePath = 파일 경로 + '/' + 파일 이름
-        BufferedReader br = new BufferedReader(new FileReader(jsonFilePath));
-        // StringBuilder : 단일 스레드
-        StringBuilder sb = new StringBuilder();
-        String jsonFileContentLine;
+    public String readFile(String jsonFilePath) throws FileNotFoundException, UnsupportedEncodingException, IOException {
+        if (isExistedFile(jsonFilePath)){
+            // jsonFilePath = 파일 경로 + '/' + 파일 이름
+            File file = new File(jsonFilePath);
+            FileInputStream fis = new FileInputStream(file);
+            InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
+            BufferedReader br = new BufferedReader(isr);
 
-        while((jsonFileContentLine = br.readLine()) != null)){
-            sb.append(jsonFileContentLine);
+            // StringBuilder : 문자열 연산이 많을 경우, 동기화를 고려하지 않기 때문에 단일 스레드 환경일 경우(StringBuffer보다 성능 뛰어남) 사용
+            StringBuilder sb = new StringBuilder();
+            String jsonFileContentLine;
+
+            while((jsonFileContentLine = br.readLine()) != null){
+                sb.append(jsonFileContentLine);
+            }
+            br.close();
+            jsonFileContent = sb.toString();
+            return jsonFileContent;
+        } else {
+            return null;
         }
-        jsonFileContent = sb.toString();
-        return jsonFileContent;
     }
+
+    private boolean isExistedFile(String jsonFilePath) {
+        File file = new File(jsonFilePath);
+        return file.exists();
+    }
+
 }
--- src/main/java/com/hanhwa/jsonparser/JSONParser.java
+++ src/main/java/com/hanhwa/jsonparser/JSONParser.java
@@ -2,7 +2,7 @@ package com.hanhwa.jsonparser;
 
 public class JSONParser {
     public void jsonParsing(String jsonFilePath, JSONFileReader jsonFileReader, ObjectBinder objectBinder){
-        String jsonFileContent = jsonFileReader.read(jsonFilePath);
+        String jsonFileContent = jsonFileReader.readFile(jsonFilePath);
         BindingObject bindingObject = objectBinder.createObject(jsonFileContent);
     }
 }
