a592ce200287f9e7530378cfc96ca6674fcda433
7ahyeon <7ahyeon@gmail.com>
2023-06-28 14:22:13 +0900

    #28 로컬 서버 요청 JSON 수신 성공

--- src/main/java/com/hanhwa/jsonparser/rsrv/controller/FrontController.java
+++ src/main/java/com/hanhwa/jsonparser/rsrv/controller/FrontController.java
@@ -5,13 +5,11 @@ import com.hanhwa.jsonparser.rsrv.model.command.MainCommand;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
 // @WebServlet() : 서블릿 자동 등록(접근 경로 맵핑 어노테이션)
 // *.do : 보안을 위하여 기존 서블릿 이름이 아닌 .do URL 패턴 호출
@@ -21,9 +19,26 @@ public class FrontController extends HttpServlet {
     // SUID(serialVersionUID) : 자바 직렬화 버전 값은 직접 관리해야 함(선언하지 않으면 클래스의 기본 해시값 사용)
     private static final long serialVersionUID = 1L;
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        resp.setContentType("application/json; charset=UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        doAction(req, resp);
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        // setCharacterEncoding : 요청 데이터 문자 타입 UTF-8 처리
+        resp.setContentType("text/html; charset=UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        doAction(req, resp);
+    }
+
+    private void doAction(HttpServletRequest req, HttpServletResponse resp) {
+//        req.setCharacterEncoding("UTF-8");
         // getServletPath : 파일명만 가져옴
-        String com = req.getServletPath();
+        String uri = req.getRequestURI();
+        String conPath = req.getContextPath();
+        String com = uri.substring(conPath.length());
+        //String com = req.getServletPath();
 
         Command command = null;
 
@@ -34,22 +49,27 @@ public class FrontController extends HttpServlet {
 
         // 커맨드 실행
         String path = command.execute(req, resp);
-        System.out.println(path);
-        // 커맨드 실행 실패시 메인 페이지 포워딩
-        if (path == null || path.equals("") ) {
+
+
+        if (path.endsWith(".jsp")) {
+            try {
+                req.getRequestDispatcher(path).forward(req,resp);
+            } catch (ServletException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            // 커맨드 실행 실패시 메인 페이지 포워딩
+        } else if (path == null || path.equals("") ) {
             // include() : 다른 자원의 수행 결과를 클라이언트로부터 요청된 Servlet 안에 포함하여 응답
             // forward() : 다른 자원의 수행 결과가 대신 클라이언트로 응답
-            req.getRequestDispatcher("/WEB-INF/view/main.jsp").forward(req, resp);
-        } else {
-            PrintWriter out = resp.getWriter();
-            out.print(path);
+            try {
+                req.getRequestDispatcher("/WEB-INF/view/main.jsp").forward(req, resp);
+            } catch (ServletException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
-    }
-
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        // setCharacterEncoding : 요청 데이터 문자 타입 UTF-8 처리
-        req.setCharacterEncoding("UTF-8");
-        doGet(req, resp);
     }
 }
--- src/main/java/com/hanhwa/jsonparser/rsrv/model/command/MainCommand.java
+++ src/main/java/com/hanhwa/jsonparser/rsrv/model/command/MainCommand.java
@@ -1,23 +1,36 @@
 package com.hanhwa.jsonparser.rsrv.model.command;
 
-import com.hanhwa.jsonparser.rsrv.Application;
-
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainCommand implements Command {
     @Override
-    public String execute(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        res.setCharacterEncoding("UTF-8");
-        res.setContentType("text/html; charset=UTF-8");
+    public String execute(HttpServletRequest req, HttpServletResponse res){
+        BufferedReader br = null;
+        StringBuffer sb = null;
+        String result = "N";
 
-        Application application = new Application();
-        String rsrvRequest = application.application();
-        PrintWriter out = res.getWriter();
-        out.print(rsrvRequest);
-        out.close();
+        try {
+            br = new BufferedReader(new InputStreamReader(req.getInputStream()));
+            String strData;
+            sb = new StringBuffer();
+            while ((strData = br.readLine()) != null) {
+                System.out.println(strData);
+                sb.append(strData);
+            }
+            br.close();
+            System.out.println(sb);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+       //try catch io servlet
         return "/WEB-INF/view/main.jsp";
     }
 }
